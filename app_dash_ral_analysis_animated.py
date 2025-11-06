bfb3599be36030fc85375ffeba9925770a463ef5
ThiagoDev622 <103607642+Thiago1927@users.noreply.github.com>
2025-11-06 18:11:04 -0300

    Update app_dash_ral_analysis_animated.py

--- app_dash_ral_analysis_animated.py
+++ app_dash_ral_analysis_animated.py
@@ -109,7 +109,7 @@ def update_output(contents, filename):
             dbc.Col(dcc.Graph(figure=graf_pie, animate=True), md=6),
         ])
     ])
-
+    
+    server = app.server
 if __name__ == "__main__":
     app.run_server(host="0.0.0.0", port=8050, debug=True)
-    server = app.server
